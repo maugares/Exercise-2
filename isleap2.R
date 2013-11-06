412e2017598dbdf8decfb42641010d1e80613bf6
unknown <garci043@D0105086.wurnet.nl>
2013-11-06 17:41:16 +0100

    Updated version excluding years before 1582

---
+++ isleap2.R
@@ -0,0 +1,27 @@
+# NAME: Mauro Garcia Esteban
+
+isleap <- function (year){
+    if (year > 1582){
+      if (year %% 400 == 0){
+        leap <- 'TRUE'
+      } 
+      else if (year %% 100 == 0){
+        leap <- 'FALSE'
+      } 
+      else if (year %% 4 == 0){
+        leap <- 'TRUE'
+      } 
+      else {
+        leap <- 'FALSE'
+      }}
+    else{
+      leap <- 'Year out of range'
+    }
+  return (leap)
+}
+
+
+
+isleap (2000)
+isleap (1999)
+isleap (1203)
